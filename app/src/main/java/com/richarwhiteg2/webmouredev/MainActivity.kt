0f820184cc6e2592d8546c9741cb0ba0a9666451
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-06-12 16:27:17 -0500

    Version 1 finalizada.

---
+++ app/src/main/java/com/richarwhiteg2/webmouredev/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.richarwhiteg2.webmouredev
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+/* Realizada por Ing. Brayan Ricardo Blanco García
+    Esta app muestra una vista web de la plataforma Jira para tickets de Mesa de ayuda de Rabbitmx.
+    Se establecio un tiempo de 6 minutos en que se está actualizando para que siempre muestre la pantalla de generar tickets.
+    El tiempo puede ser modificado en la variable tiempoSegundos
+ */
+
+
+class MainActivity : AppCompatActivity() {
+
+    //Private
+    private var webView: WebView?=null
+
+    //la pagina que carga siempre el webview
+    private val BASE_URL = "https://rabbit-mx.atlassian.net/servicedesk/customer/portal/6"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "CARGANDO...UN MOMENTO POR FAVOR." , Toast.LENGTH_LONG).show()
+
+        //WebView
+        webView = findViewById(R.id.webView)
+        webView?.webViewClient = WebViewClient()
+        webView?.clearCache(true) //false
+        webView?.settings?.javaScriptEnabled=true
+        webView?.loadUrl(BASE_URL)
+    }
+
+    //Temporizador para actualizar la activity.
+    fun temporizador(){
+        val recargar = Intent(this, MainActivity::class.java) //para recargar activity
+
+        //En la variable tiempoSegundos se ingresa la cantidad de tiempo para que se recargue la activity.
+        val tiempoSegundos = 600
+        val tiempoMilisegundos = (tiempoSegundos.toLong())*1000
+
+
+        object : CountDownTimer(tiempoMilisegundos, 1000) {
+            override fun onTick(p0: Long) {
+                val cuentaSegundos = (p0/1000).toInt()
+            }
+            override fun onFinish() {
+                startActivity(recargar)
+            }
+        }.start()
+    }
+    fun cargar(view: View){
+        val recargar = Intent(this, MainActivity::class.java)
+        temporizador()
+        startActivity(recargar)
+    }
+}
